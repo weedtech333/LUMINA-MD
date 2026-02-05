2813f7706323c890f5f01cc1124acf42fb681650
weedtech333 <weedtech333@gmail.com>
2026-02-05 17:05:43 -0500

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,13 +7,13 @@ export async function uptime(client, message) {
     const minutes = Math.floor((uptime % 3600) / 60)
     const seconds = Math.floor(uptime % 60)
     
-    const text = `┌─🤖 DIGITAL CREW 243 ─┐
+    const text = `┌─⏰️ LUMINA MD 🚀─┐
 │
 │ ⏱️ Uptime: ${days}d ${hours}h ${minutes}m
 │ 💾 RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(1)}MB
 │
 │ "Beyond limits, we rise."
-│     - DC243 -
+│     - Lumina-
 └────────────────────┘`
     
     await client.sendMessage(remoteJid, { text: text })
