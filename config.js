bbd12cdf3c41c8f202c645b869e986cf67266930
weedtech333 <weedtech333@gmail.com>
2026-02-05 10:30:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "243833389567" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "18494869204" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
